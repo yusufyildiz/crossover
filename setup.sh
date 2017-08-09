eab230b6f4a6a757a8cfd2d2daba89bd090a5a97
root <root@ip-172-31-13-115.eu-central-1.compute.internal>
2017-08-09 21:47:57 +0000

    fixed a bug

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ wget -qO- https://get.docker.com/ | sh
 
 # pull the docker images & run
 docker pull mysql
-docker run -p 3900:3306 --name mysql -e MYSQL_ROOT_PASSWORD=toor -d mysql:latest
+docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=toor -d mysql:latest
 docker pull tutum/apache-php
 docker build -t tutum/apache-php .
 docker run -d -p 80:80 --name apache --link mysql:mysql -v /var/www/html:/var/www/html tutum/apache-php:latest
